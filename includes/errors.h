a8bee829ac50da74cfacc6927fc76df606fda482
Bogdan_lxn <bogdan.plockiy@mail.ru>
2024-02-22 20:03:37 +0300

    all functions are divided in 2 files: .h and .c

--- includes/errors.h
+++ includes/errors.h
@@ -1,6 +1,8 @@
 #ifndef ERRORS_H
 #define ERRORS_H
 
+#include <stdio.h>
+
 // возможные ошибки при работе программы
 // ФАЙЛЫ
 #define OK                          (0)
@@ -18,28 +20,6 @@
 if(src & er_code)str = #er_code;
 
 // функция печати ошибок
-void errorPrint(int er)
-{
-    if(er != OK)
-    {
-        const char* er_msg = "\0";
-
-        // проверка на каждый код ошибки
-        // IF_ERR(er, FILE_DOESNT_EXIST)
-        //     er_msg = "FILE_DOESNT_EXIST";
-        
-        // IF_ERR(er, FILE_HAS_NO_DESCRIPTOR)
-        //     er_msg = "FILE_HAS_NO_DESCRIPTOR";
-        
-        CR_ERR_MSG(er_msg, er, FILE_DOESNT_EXIST);
-        CR_ERR_MSG(er_msg, er, FILE_HAS_NO_DESCRIPTOR);
-        CR_ERR_MSG(er_msg, er, FILE_HAS_NO_PATH);
-        CR_ERR_MSG(er_msg, er, FILE_HAS_NO_SIZE);
-        CR_ERR_MSG(er_msg, er, FILE_INCORRECT_INPUT_SIZE);
-        //CR_ERR_MSG(er_msg, er, FILE_DOESNT_EXIST);
-        
-        fprintf(stderr, "error msg: <%s>\n", er_msg);
-    }
-}
+void errorPrint(int er);
 
 #endif // !ERRORS_H
