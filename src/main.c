55ba5c865e5bea1cd6e48c93780bb31eb3ff1235
AKTOETO <bogdan.plockiy@mail.ru>
2024-03-24 22:32:16 +0300

    added ability to open different images by using argv

--- src/main.c
+++ src/main.c
@@ -1,15 +1,27 @@
 #include "image.h"
 
+char *g_in_file = "1.png";
+char *g_out_file = "sobel.png";
+
 int main(int argc, char **argv)
 {
+    char *input_file = g_in_file;
+    char *output_file = g_out_file;
 
+    // если введен входной файл
+    if(argc > 1)
+        input_file = argv[1];
+    
+    // если введен выходной файл
+    if(argc > 2)
+        output_file = argv[2];
 
     Image img, img2;
-    imageLoad(&img, "4.png");
+    imageLoad(&img, input_file);
     imageInfo(&img);
 
     imageApplySobel(&img, &img2);
-    imageSave(&img2, "sobel.png");
+    imageSave(&img2, output_file);
     
     imageFree(&img);
     imageFree(&img2);
